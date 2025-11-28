4b1d3866def5afc9a7bf7525babcb3d08fca9ae3
Agatha Soares Evangelista <54553786869@imaccommand031.local>
2025-11-28 13:38:59 -0300

    apredendo o array

---
+++ Array/array.js
@@ -0,0 +1,35 @@
+// let frutas = ["Goiaba", "Morango", "Melancia", "Uva", "Pera"]; //array/lista vazia
+
+// //length igual a comprimento, ou seja, a quantidade de itens que q lista tem.
+// console.log("A lista de frutas tem " + frutas.length + " itens.");
+
+// console.log("A primeira fruta é a: " + frutas[0]);
+
+// // acesse o item 4 da lista e exiba-o no console
+// console.log("A quarta fruta é a: " + frutas[3]);
+
+// //exibe todas as frutas do array e com o seu formato
+// console.log(frutas);
+// for(let i = 0; i <= frutas.length; i++){
+//      console.log(frutas[i])
+// }
+
+//crie uma lisat de tarefas, contendono max  10 intens
+//E exiba cada tarefas
+
+let tarefas = ["Estudar", "Arrumar o quarto", "Lavar a louça", "Fazer o trabalho", "Tomar banho", "Jogar", "Ler um livro", "Organizar a mochila", "Dormir", "Treinar"];
+
+//length igual a comprimento, ou seja, a quantidade de itens que a lista tem.
+console.log("A lista de tarefas tem " + tarefas.length + " itens.");
+
+console.log("A primeira tarefa é: " + tarefas[0]);
+
+// acesse o item 4 da lista e exiba-o no console
+console.log("A quarta tarefa é: " + tarefas[3]);
+
+//exibe todas as tarefas do array e com o seu formato
+console.log(tarefas);
+
+for (let i = 0; i <= tarefas.length; i++) {
+    console.log(tarefas[i]);
+}
